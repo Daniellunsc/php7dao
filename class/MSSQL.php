15f5c8ce11c333635866c15ad10f2c48fb866383
Daniel Luna <daniellunsc@gmail.com>
2017-08-21 14:21:25 -0300

    Criação do projeto

---
+++ class/MSSQL.php
@@ -0,0 +1,49 @@
+<?php 
+
+class MSSQL extends PDO {
+
+    private $conn;
+
+    public function __construct(){
+        $this->conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
+    }
+
+    private function setParams($statment, $parameters = array()){
+
+        foreach($parameters as $key => $value){
+            
+            $statment->bindParam($key, $value);
+            
+        }
+        
+    }
+
+    private function setParam($statment, $key, $value){
+
+        $statment->bindParam($key, $value);
+
+    }
+
+    public function query($RawQuery, $params = array()){
+
+        $stmt = $this->conn->prepare($RawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function select($RawQuery, $params = array()){
+
+        $stmt = $this->query($RawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+}
+
+
+?>
